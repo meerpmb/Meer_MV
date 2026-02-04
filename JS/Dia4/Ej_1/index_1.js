a150b20042b6a322fa72e7e314976a7e75f9dceb
meerpmb <meerpmb@gmail.com>
2026-02-04 19:54:19 +0100

    04-02

---
+++ JS/Dia4/Ej_1/index_1.js
@@ -0,0 +1,7 @@
+const base = 23
+const ancho = 16
+const altura = 13
+
+const resultado = base*ancho*altura
+
+console.log(resultado)
---
+++ JS/Dia4/Ej_2_Arrays/index_2.js
@@ -0,0 +1,7 @@
+let hobbys = ['Musica', 'Jugar al rugby', 'Tocar la guitarra', 'Leer']
+
+console.log(hobbys[1])
+
+hobbys.push('Terminar la clase')
+
+console.log(hobbys)
