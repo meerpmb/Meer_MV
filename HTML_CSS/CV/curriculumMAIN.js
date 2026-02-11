a1cc422c05259b6299d2fde845cfc6bec482758a
meerpmb <meerpmb@gmail.com>
2026-02-11 17:29:43 +0100

    11-02-class-last

--- HTML_CSS/CV/curriculumMAIN.js
+++ HTML_CSS/CV/curriculumMAIN.js
@@ -1,14 +1,30 @@
+const botonEnviar = document.getElementById('boton-enviar')
+let name = document.getElementById('nombre')
+let message = document.getElementById('correo')
 
-(function(){
-    emailjs.init("TU_USER_ID");
-})();
 
-document.getElementById('formulario-contacto').addEventListener('submit', function(event) {
-    event.preventDefault();
-    emailjs.sendForm('TU_SERVICE_ID', 'TU_TEMPLATE_ID', this)
-        .then(function() {
-            alert('¡Correo enviado con éxito!');
-        }, function(error) {
-            alert('Error al enviar: ' + JSON.stringify(error));
-        });
-});
+function botonEnviarHandler(eventoClick){
+
+    console.log('${nombre} Gracias por tu mensaje ${correo}, me pondré en contacto contigo en cuanto pueda.')
+
+}
+
+botonEnviar.addEventListener('click', botonEnviarHandler)
+
+
+
+
+// (function(){
+//     emailjs.init("meerpmb@gmail.com");
+//     // entiendo que tengo que conectar el emailjs al html, pero no se como :/
+// })();
+
+// document.getElementById('formulario-contacto').addEventListener('submit', function(event) {
+//     event.preventDefault();
+//     emailjs.sendForm('service_60y1vy5', 'template_jl7pz34', this)
+//         .then(function() {
+//             alert('¡Correo enviado con éxito!');
+//         }, function(error) {
+//             alert('Error al enviar: ' + JSON.stringify(error));
+//         });
+// });
